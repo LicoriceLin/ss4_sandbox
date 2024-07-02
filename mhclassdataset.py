285025464671b45c0a84b6379e10ff4fa10e8fdc
Ubuntu <azureuser@SS4Master.uibvrcyr1tgullot4a3rubyena.bx.internal.cloudapp.net>
2024-07-02 13:02:01 +0000

    update dataset

--- mhclassdataset.py
+++ mhclassdataset.py
@@ -9,7 +9,8 @@ from torch.utils.data import DataLoader,Dataset
 from functools import partial
 import torch
 from transformers import PreTrainedTokenizer,EsmTokenizer
-from collections.abc import Sequence
+from collections.abc import Sequence,Mapping,Callable
+from argparse import ArgumentParser,Namespace
 NUM_CLASS=36
 ITEM=Dict[str,Union[str,List[float]]]
 
@@ -21,8 +22,8 @@ torch_generator.manual_seed(seed)
 model_name='facebook/esm2_t12_35M_UR50D'
 tokenizer:PreTrainedTokenizer=EsmTokenizer.from_pretrained(model_name)
 
-torch.nn.CrossEntropyLoss
 def create_mu_map():
+    NUM_CLASS=36
     mapping = {}
     for i, char in enumerate(range(ord('A'), ord('Z') + 1)):
         mapping[chr(char)] = i
@@ -39,79 +40,125 @@ def create_map(tokens:Sequence):
     ret={ t:i for i,t in enumerate(tokens)}
 
 class MHClassDataset(Dataset):
-    def __init__(self,dataframe:pd.DataFrame,
+    def __init__(self,
+        dataset_path:Optional[str]=None,
+        dataframe:Optional[pd.DataFrame]=None,
         feature_col:str="seq",label_cols:List[str]=['mu'],
-        ignore_labels=[],ignore_index=-100
-        ) -> None: #,label_maps:Optional[Dict[str,Dict[str]]]=None
+        ignore_labels=[],
+        ignore_index=-100,
+        max_length=800,
+        label_maps:Optional[Dict[str,Dict[str,Mapping]]]=None, #{'mu':MU_MAP}
+        ) -> None:
+        #TODO: config system to manage parameters
         super().__init__()
+        # --
         self.feature_col=feature_col
         self.label_cols=label_cols
         self.ignore_labels=ignore_labels
         self.ignore_index=ignore_index
-        self.dataframe=dataframe
-        
-
+        self.max_length=max_length
+        # --
+        if dataset_path is not None:
+            self.innner_dataset=ds.load_from_disk(dataset_path).take(10000)
+        elif dataframe:
+        # self.dataframe=dataframe
+            self.innner_dataset=ds.from_pandas(dataframe).take(10000)
+        else:
+            raise ValueError('either `dataset_path` or `dataframe` should be assigned.')
+        def ds_filter(x):
+            if len(x[feature_col])<3:
+                return False
+            else:
+                for i in label_cols:
+                    if i not in x:
+                        return False
+            return True
+        self.innner_dataset=self.innner_dataset.filter(ds_filter)
+        # --
+        if label_maps is None:
+            self._init_labels()
+        else:
+            self.label_counts=defaultdict(lambda:defaultdict(int))
+            self.label_maps=label_maps
+            
+        self.innner_dataset=self.innner_dataset.map(self.map_fn)
+            
     def _init_labels(self):
-        self.label_count=defaultdict(lambda:defaultdict(int))
-        self.label_map=defaultdict(lambda:defaultdict(int))
-        for label_col in self.label_cols:
-            for label_seq in self.dataframe[label_col]:
-                for label in label_seq:
-                    self.label_count[label_col][label]+=1
-            for i,t in enumerate(self.label_count[label_col].keys()):
+        self.label_counts=defaultdict(lambda:defaultdict(int))
+        self.label_maps=defaultdict(lambda:defaultdict(int))
+        for item in self.innner_dataset.iter():
+            for label_col in self.label_cols:
+            # for label_seq in self.dataframe[label_col]:
+                for label in item[label_col]:
+                    self.label_counts[label_col][label]+=1
+            for i,t in enumerate(self.label_counts[label_col].keys()):
                 if t not in self.ignore_labels:
-                    self.label_map[label_col][t]=i
+                    self.label_maps[label_col][t]=i
 
         
-        innner_dataset=ds.from_pandas(dataframe)
+        
         
         
     @property
-    def collate_fn(self):
-        return partial(collate_fn,feature=self.feature,labels=self.labels)
+    def collate_fn(self)->Callable:
+        '''
+        WARNING: `train` should be specified during downstream pipelines.
+        '''
+        def collate_fn(items:List[ITEM],train:bool,max_length=800,mask_rate=0.1,feature_col:str="seq",label_cols:List[str]=['mu']):
+            ret=defaultdict(list)
+            for item in items:
+                del_len=len(item['input_ids'])-max_length
+                if del_len<0:
+                    def padding(item,del_len):
+                        item['input_ids']=item['input_ids']+[1]*(-del_len)
+                        item['attention_mask']=item['attention_mask']+[0]*(-del_len)
+                        for label in label_cols:
+                            item[f'{label}_ids']=item[f'{label}_ids']+[-100]*(-del_len)
+                        item['idx_b']=0
+                    padding(item,del_len)
+                elif del_len>0:
+                    idx_b=random_generator.randint(0,del_len)
+                    truncation=lambda x:[x[0]]+x[idx_b+1:idx_b+max_length-1]+[x[-1]]
+                    item['input_ids']=truncation(item['input_ids'])
+                    item['attention_mask']=truncation(item['attention_mask'])
+                    for label in label_cols:
+                        item[f'{label}_ids']=truncation(item[f'{label}_ids'])
+                    item['idx_b']=idx_b+1
+                else:
+                    item['idx_b']=0
+                
+                for k in item.keys():
+                    ret[k].append(item[k])
+                    
+            for k in ['input_ids','attention_mask','idx_b']+[f'{label}_ids' for label in feature_col]:
+                ret[k]=torch.tensor(ret[k],dtype=torch.long)
+                
+            ret['position_ids']=create_position_ids(
+                ret['input_ids'],ret['attention_mask'],ret['idx_b'].reshape(-1,1))
+            if train:
+                _=ret['input_ids'][:,1:-1]
+                _[torch.rand_like(_,dtype=torch.float16)>1-mask_rate]=tokenizer.mask_token_id
+            return ret
+        return partial(collate_fn,max_length=self.max_length,feature_col=self.feature_col,label_cols=self.label_cols)
     
-    
-def map_fn(item,feature_col:str="seq",label_cols:List[str]=['mu'],label_maps:Dict[str,Dict[str]]={'mu':MU_MAP}):
-    tokenized_inputs = tokenizer(item[feature_col], truncation=False,padding=True)
-    item.update(tokenized_inputs)
-    for label in label_cols:
-        item[f'{label}_ids']=[-100]+[label_maps['label'].get(i,-1) for i in item[label]]+[-100]
-    return item
-
-
-def collate_fn(items:List[ITEM],max_length=800,train:bool=True,mask_rate=0.1,feature_col:str="seq",label_cols:List[str]=['mu']):
-    ret=defaultdict(list)
-    for item in items:
-        del_len=len(item['input_ids'])-max_length
-        if del_len<0:
-            def padding(item,del_len):
-                item['input_ids']=item['input_ids']+[1]*(-del_len)
-                item['attention_mask']=item['attention_mask']+[0]*(-del_len)
-                for label in label_cols:
-                    item[f'{label}_ids']=item[f'{label}_ids']+[-100]*(-del_len)
-                item['idx_b']=0
-            padding(item,del_len)
-        elif del_len>0:
-            idx_b=random_generator.randint(0,del_len)
-            truncation=lambda x:[x[0]]+x[idx_b+1:idx_b+max_length-1]+[x[-1]]
-            item['input_ids']=truncation(item['input_ids'])
-            item['attention_mask']=truncation(item['attention_mask'])
+    @property
+    def map_fn(self)->Callable:
+        def map_fn(item:Dict[str,str],feature_col:str="seq",
+                label_cols:List[str]=['mu'],
+                label_maps:Dict[str,Dict[str,Mapping]]={'mu':MU_MAP},
+                ignore_labels=[],
+                ignore_index=-100):
+            tokenized_inputs = tokenizer(item[feature_col], truncation=False,padding=True)
+            item.update(tokenized_inputs)
             for label in label_cols:
-                item[f'{label}_ids']=truncation(item[f'{label}_ids'])
-            item['idx_b']=idx_b+1
-        else:
-            item['idx_b']=0
-        
-        for k in item.keys():
-            ret[k].append(item[k])
-            
-    for k in ['input_ids','attention_mask','idx_b']+[f'{label}_ids' for label in feature_col]:
-        ret[k]=torch.tensor(ret[k],dtype=torch.long)
-        
-    ret['position_ids']=create_position_ids(
-        ret['input_ids'],ret['attention_mask'],ret['idx_b'].reshape(-1,1))
-    if train:
-        _=ret['input_ids'][:,1:-1]
-        _[torch.rand_like(_,dtype=torch.float16)>1-mask_rate]=tokenizer.mask_token_id
-    return ret
+                item[f'{label}_ids']=[ignore_index]+[label_maps[label].get(i,-1) for i in item[label]]+[ignore_index]
+            for label in ignore_labels:
+                item.pop(label)
+            return item
+        return partial(map_fn,feature_col=self.feature_col,label_cols=self.label_cols,
+                       label_maps=self.label_maps,ignore_labels=self.ignore_labels,
+                       ignore_index=self.ignore_index)
+
+
+
         
--- tmp_train_2.py
+++ tmp_train_2.py
@@ -59,10 +59,11 @@ torch_generator.manual_seed(seed)
 model_name='facebook/esm2_t12_35M_UR50D' #'facebook/esm2_t6_8M_UR50D'
 tokenizer:EsmTokenizer=EsmTokenizer.from_pretrained(model_name)
 
+mu='Mu'
 def map_fn(examples):
     tokenized_inputs = tokenizer(examples["seq"], truncation=False,padding=True)
     examples.update(tokenized_inputs)
-    examples['mu_ids']=[-100]+[OUTPUT_MAP.get(i,-1) for i in examples['mu']]+[-100]
+    examples[f'{mu}_ids']=[-100]+[OUTPUT_MAP.get(i,-1) for i in examples[mu]]+[-100]
     return examples
 
 def collate_fn(features:List[ITEM],max_length=800,train:bool=True,mask_rate=0.1):
@@ -73,7 +74,7 @@ def collate_fn(features:List[ITEM],max_length=800,train:bool=True,mask_rate=0.1)
             def padding(feature,del_len):
                 feature['input_ids']=feature['input_ids']+[1]*(-del_len)
                 feature['attention_mask']=feature['attention_mask']+[0]*(-del_len)
-                feature['mu_ids']=feature['mu_ids']+[-100]*(-del_len)
+                feature[f'{mu}_ids']=feature[f'{mu}_ids']+[-100]*(-del_len)
                 feature['idx_b']=0
             padding(feature,del_len)
         elif del_len>0:
@@ -81,7 +82,7 @@ def collate_fn(features:List[ITEM],max_length=800,train:bool=True,mask_rate=0.1)
             truncation=lambda x:[x[0]]+x[idx_b+1:idx_b+max_length-1]+[x[-1]]
             feature['input_ids']=truncation(feature['input_ids'])
             feature['attention_mask']=truncation(feature['attention_mask'])
-            feature['mu_ids']=truncation(feature['mu_ids'])
+            feature[f'{mu}_ids']=truncation(feature[f'{mu}_ids'])
             feature['idx_b']=idx_b+1
         else:
             feature['idx_b']=0
@@ -89,7 +90,7 @@ def collate_fn(features:List[ITEM],max_length=800,train:bool=True,mask_rate=0.1)
         for k in feature.keys():
             ret[k].append(feature[k])
             
-    for k in ['input_ids','attention_mask','mu_ids','idx_b']:
+    for k in ['input_ids','attention_mask',f'{mu}_ids','idx_b']:
         ret[k]=torch.tensor(ret[k],dtype=torch.long)
         
     ret['position_ids']=create_position_ids(
@@ -99,31 +100,33 @@ def collate_fn(features:List[ITEM],max_length=800,train:bool=True,mask_rate=0.1)
         _[torch.rand_like(_,dtype=torch.float16)>1-mask_rate]=tokenizer.mask_token_id
     return ret
         
-mu=pd.read_pickle('data/mu.pkl')
-mu=mu[mu['seq'].apply(lambda x:len(x))>0]
-dataset=Dataset.from_pandas(mu).map(map_fn)
+# mu=pd.read_pickle('data/mu.pkl')
+# mu=mu[mu['seq'].apply(lambda x:len(x))>0]
+# dataset=Dataset.from_pandas(mu).map(map_fn)
+dataset=Dataset.load_from_disk('data/af_swiss_muonly')
 loader=DataLoader(dataset,batch_size=8,collate_fn=partial(collate_fn,max_length=600,train=True,mask_rate=0.1))
 
 # %%
 device=0
-epoch=20
+epoch=0
 config=EsmConfig.from_pretrained(model_name)
 config.num_labels=NUM_CLASS
-model=EsmForTokenClassification.from_pretrained(model_name,config=config).to(device)
+model=EsmForTokenClassification(config=config)
+model.load_state_dict(torch.load('data/ep-99.pt'))
+model.to(device)
 model.train()
 # model.load_state_dict(torch.load('train/esm2_t12_240625-152559_seed42/ep-19.pt'))
 torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-optimizer = Adam(model.parameters(), lr=1e-5,weight_decay=1e-7)
+optimizer = Adam(model.parameters(), lr=1e-6,weight_decay=1e-7)
 # scheduler = OneCycleLR(optimizer, max_lr=5e-5, steps_per_epoch=len(loader), epochs=epoch)
 # schedulers = [ConstantLR(optimizer, factor=1e-2, total_iters=1),
 #               ConstantLR(optimizer, factor=1e2, total_iters=1),
 #               ExponentialLR(optimizer, gamma=0.95)]
 # scheduler = ChainedScheduler(schedulers)\
 # scheduler = ExponentialLR(optimizer, gamma=0.95)
-schedulers = [ConstantLR(optimizer, factor=1e-2, total_iters=1),
-              ConstantLR(optimizer, factor=1e-1, total_iters=1),
+schedulers = [ConstantLR(optimizer, factor=1e-1, total_iters=1),
               ExponentialLR(optimizer, gamma=0.95)]
-scheduler = SequentialLR(optimizer,schedulers,[1,2])
+scheduler = SequentialLR(optimizer=optimizer,schedulers=schedulers,milestones=[1])
 criterion=nn.CrossEntropyLoss()
 metrics=MulticlassMetric(num_classes=NUM_CLASS)
 # %%
@@ -135,15 +138,13 @@ writer = SummaryWriter(log_dir=f'{odir}/log')
 step=0
 def train_epoch():
     global step,losses
-    
-    
     for iteration, (batch) in enumerate(loader):
         optimizer.zero_grad()
         pred=model(batch['input_ids'].to(device),
                    batch['attention_mask'].to(device),
                    position_ids=batch['position_ids'].to(device)).logits
         # pred=nn.functional.softmax(pred,-1)
-        target=batch['mu_ids'].to(device)
+        target=batch[f'{mu}_ids'].to(device)
         loss:torch.Tensor = criterion(pred.permute(0, 2, 1),target)
         if loss.item()<5:
             loss.backward()
@@ -158,7 +159,7 @@ def train_epoch():
         # writer.add_scalar(f"loss", loss.item(), step)
         # import pdb;pdb.set_trace()
         
-        if step%50==0:
+        if step%100==0:
             result = metrics.compute()
             step_log = f"training: [{iteration}/{len(loader)}]\t"
             l_=sum(losses)/len(losses)
@@ -176,10 +177,12 @@ def train_epoch():
             metrics.reset()
             print(step_log)
             
-for epoch in range(0,100):
+        if step%5000==0:
+            torch.save(model.state_dict(), f'{odir}/step-{step}.pt')
+            scheduler.step()
+                
+for epoch in range(epoch,epoch+20):
     losses=[]
     writer.add_scalar("epoch", epoch, step)
     train_epoch()
-    scheduler.step()
-    if epoch%3==0:
-        torch.save(model.state_dict(), f'{odir}/ep-{epoch}.pt')
+    
